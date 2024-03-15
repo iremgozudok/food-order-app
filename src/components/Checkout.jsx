3b8bacbda784e7854c065b597888818e00c01bd4
iremserenay <irem@serenay.net.tr>
2024-03-15 17:30:08 +0300

    handling form submission and validation

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -9,16 +9,26 @@ import UserProgressContext from "../store/UserProgressContext";
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
+
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
+
   function handleClose() {
     userProgressCtx.hideCheckout();
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries()); // { "full-name": "John Doe", "email": "john@example", ... }
+  }
+
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)} </p>
         <Input label="Full Name" type="text" id="full-name" />
